533ef7a1d6ee61d27a3eca717754bf6171b8a101
cindyparkk <cindypark31@gmail.com>
2025-07-02 23:44:26 +0900

    installed styled-comps, axios

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>Daemyung-Realco.</h1>
+    </div>
+  );
+}
