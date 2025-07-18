070ecfb041a993d424e9542714e6a03ef485a99b
cindyparkk <cindypark31@gmail.com>
2025-07-18 11:50:11 +0900

    wrapped up projects page

--- src/app/projects/[params]/page.jsx
+++ src/app/projects/[params]/page.jsx
@@ -4,8 +4,14 @@ import { useState, useEffect, useRef } from "react";
 import { useParams, useRouter } from "next/navigation";
 
 import Title from "../../../components/title";
-
 import colors from "../../../constants/colors";
+
+import {
+  RealEstateData,
+  FNBData,
+  EntertainmentData1,
+  EntertainmentData2,
+} from "../../../data/projects";
 
 const sections = ["real-estate", "fnb", "entertainment"];
 
@@ -14,69 +20,98 @@ const ProjectsPage = () => {
   const sectionRefs = useRef([]);
   const router = useRouter();
   const { params } = useParams(); // This gets the dynamic [params]
-  const [currentIndex, setCurrentIndex] = useState(() => {
-    const index = sections.indexOf(params);
-    return index !== -1 ? index : 0;
-  });
+  // const [currentIndex, setCurrentIndex] = useState(() => {
+  //   const index = sections.indexOf(params);
+  //   return index !== -1 ? index : 0;
+  // });
 
-  // On load: scroll to the section based on the URL param
+  // // On load: scroll to the section based on the URL param
+  // useEffect(() => {
+  //   const index = sections.indexOf(params);
+  //   if (index !== -1 && sectionRefs.current[index]) {
+  //     sectionRefs.current[index].scrollIntoView({ behavior: "auto" });
+  //     setCurrentIndex(index);
+  //   }
+  // }, [params]);
+
+  // // Throttle scroll handling with a timer to avoid spamming
+  // const scrollTimeout = useRef(null);
+
+  // const handleScroll = () => {
+  //   if (scrollTimeout.current) clearTimeout(scrollTimeout.current);
+
+  //   scrollTimeout.current = setTimeout(() => {
+  //     const scrollTop = containerRef.current.scrollTop;
+  //     const height = window.innerHeight;
+  //     const index = Math.round(scrollTop / height);
+
+  //     if (index !== currentIndex) {
+  //       setCurrentIndex(index);
+  //       router.replace(`/projects/${sections[index]}`);
+  //     }
+  //   }, 100); // Wait 100ms after scroll ends
+  // };
+
+  const [data, setData] = useState([]);
+  const [enterPage, setEnterPage] = useState(0);
+
   useEffect(() => {
-    const index = sections.indexOf(params);
-    if (index !== -1 && sectionRefs.current[index]) {
-      sectionRefs.current[index].scrollIntoView({ behavior: "auto" });
-      setCurrentIndex(index);
+    switch (params) {
+      case "real-estate":
+        setData([]);
+        break;
+      case "fnb":
+        setData(FNBData);
+        break;
+      case "entertainment":
+        if (enterPage === 0) setData(EntertainmentData1);
+        else if (enterPage === 1) setData(EntertainmentData2);
+        break;
+      default:
+        setData(RealEstateData);
     }
-  }, [params]);
+  }, [params, enterPage]);
 
-  // Throttle scroll handling with a timer to avoid spamming
-  const scrollTimeout = useRef(null);
-
-  const handleScroll = () => {
-    if (scrollTimeout.current) clearTimeout(scrollTimeout.current);
-
-    scrollTimeout.current = setTimeout(() => {
-      const scrollTop = containerRef.current.scrollTop;
-      const height = window.innerHeight;
-      const index = Math.round(scrollTop / height);
-
-      if (index !== currentIndex) {
-        setCurrentIndex(index);
-        router.replace(`/projects/${sections[index]}`);
-      }
-    }, 100); // Wait 100ms after scroll ends
+  const sliceSentences = (text) => {
+    if (text && text !== "") {
+      return text.replace(/\./g, ".\n");
+    } else return text;
   };
 
-  // Example: determine content by params
-  let content;
-  switch (params) {
-    case "real-estate":
-      content = <div>Real Estate projects content...</div>;
-      break;
-    case "fnb":
-      content = <div>Food & Beverage projects content...</div>;
-      break;
-    case "entertainment":
-      content = <div>Entertainment projects content...</div>;
-      break;
-    default:
-      content = <div>Some other projects content...</div>;
-  }
+  console.log(enterPage);
 
   // Use 'page' to change content dynamically
   return (
     <>
-      <ScrollContainer ref={containerRef} onScroll={handleScroll}>
+      {/* <ScrollContainer ref={containerRef} onScroll={handleScroll}>
         {sections.map((section, i) => (
-          <Section
-            key={section}
-            ref={(el) => (sectionRefs.current[i] = el)}
-            $bg={section}
-          >
-            <SectionContent>{section.toUpperCase()} CONTENT</SectionContent>
-          </Section>
-        ))}
-      </ScrollContainer>
+          <Section key={section} ref={(el) => (sectionRefs.current[i] = el)}> */}
       {/* <Title
+        text={
+          section === "entertainment"
+            ? "엔터테인먼트"
+            : section === "fnb"
+              ? "F&B"
+              : "분양대행"
+        }
+        hr
+        subtitle={
+          section === "entertainment" ? (
+            <>
+              대명그룹이 책임지고 담당한 <span>엔터</span> 사업
+            </>
+          ) : section === "fnb" ? (
+            <>
+              대명그룹이 책임지고 담당한 <span>F&B</span> 사업
+            </>
+          ) : (
+            <>
+              대명그룹이 책임지고 담당한 <span>분양대행</span>
+            </>
+          )
+        }
+      /> */}
+      <Title
         text={
           params === "entertainment"
             ? "엔터테인먼트"
@@ -100,7 +135,69 @@ const ProjectsPage = () => {
             </>
           )
         }
-      /> */}
+      />
+      <PageContainer>
+        {/* {section === "real-estate" && ( */}
+        {params === "real-estate" && (
+          <EmptyBox>
+            <p>[컨텐츠 업데이트 되는대로 추가 예정]</p>
+          </EmptyBox>
+        )}
+        <LogoBox>
+          {data?.logo?.length > 0 &&
+            data.logo.map((logo, idx) => (
+              <Logo
+                key={idx}
+                src={logo.src}
+                alt={logo.alt}
+                $isActive={logo.isActive}
+                onClick={() => {
+                  if (data?.logo?.length > 1) {
+                    setEnterPage(idx);
+                  }
+                }}
+              />
+            ))}
+        </LogoBox>
+        <Text>{sliceSentences(data?.intro)}</Text>
+        <BannerImageWrapper>
+          <Image src={data?.bannerImage?.src} />
+        </BannerImageWrapper>
+        <TextWrapper>
+          <p style={{ color: colors.textGrey, fontSize: "14px" }}>
+            {data?.brand?.desc}
+          </p>
+          <BrandName>
+            <h4>{data?.brand?.name}</h4>
+            <h6>{data?.brand?.location}</h6>
+          </BrandName>
+        </TextWrapper>
+        <TextWrapper>
+          {data?.desc?.map((desc, idx) =>
+            data?.isUl ? (
+              <ListedDesc key={idx}>
+                <li>{desc}</li>
+              </ListedDesc>
+            ) : (
+              <Text key={idx}>{desc}</Text>
+            )
+          )}
+        </TextWrapper>
+        {data?.images?.length > 0 && (
+          <ImageWrapperSection>
+            <Circle />
+            <RedSquare />
+            <ImageWrapper>
+              {data?.images?.map((img, idx) => (
+                <Image key={idx} src={img.src} alt={img.alt} />
+              ))}
+            </ImageWrapper>
+          </ImageWrapperSection>
+        )}
+      </PageContainer>
+      {/* </Section>
+        ))}
+      </ScrollContainer> */}
     </>
   );
 };
@@ -116,17 +213,10 @@ const ScrollContainer = styled.div`
 `;
 
 const Section = styled.div`
+  padding-top: 64px;
   height: 100vh;
   width: 100%;
   scroll-snap-align: start;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const SectionContent = styled.div`
-  font-size: 2rem;
-  font-weight: bold;
 `;
 
 const PageContainer = styled.div`
@@ -135,5 +225,158 @@ const PageContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 0px 50px 50px 50px;
+  padding-bottom: 50px;
+  gap: 20px;
+`;
+
+const EmptyBox = styled.div`
+  width: 100%;
+  background-color: ${colors.lightGrey};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 320px;
+`;
+
+const LogoBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  height: 80px;
+  width: auto;
+  overflow: hidden;
+`;
+
+const Logo = styled.img`
+  height: 100%;
+  width: auto;
+  display: block;
+  opacity: ${(props) => (props.$isActive ? 1 : 0.5)};
+
+  &:hover {
+    opacity: 1;
+    transition: opacity 0.2s ease-in-out;
+    cursor: ${(props) => !props.$isActive && "pointer"};
+  }
+`;
+
+const Text = styled.p`
+  width: 55%;
+  font-size: 14px;
+  text-align: center;
+  padding: 10px 0px;
+  white-space: pre-line;
+  line-height: 1.75;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  display: block;
+  object-fit: cover;
+  z-index: 3;
+`;
+
+const BannerImageWrapper = styled.div`
+  width: 100%;
+  max-height: 500px;
+  overflow: hidden;
+  /* margin-top: 50px; */
+  margin: 20px 0px;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BrandName = styled.div`
+  /* padding-bottom: 20px; */
+  display: flex;
+  align-items: center;
+
+  h4 {
+    color: ${colors.red};
+    font-weight: bold;
+    font-size: 28px;
+    margin-right: 15px;
+  }
+
+  h6 {
+    font-size: 14px;
+  }
+`;
+
+const ListedDesc = styled.ul`
+  li {
+    font-size: 14px;
+    margin-bottom: 4px;
+  }
+`;
+
+const ImageWrapperSection = styled.section`
+  background-color: ${colors.black};
+  position: relative;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 50px 0px;
+  margin: 50px 0px 80px 0px;
+`;
+
+const ImageWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr); /* Always 2 columns */
+  gap: 16px;
+  width: 60%;
+  z-index: 3;
+`;
+
+const RedSquare = styled.div`
+  width: 10rem;
+  height: 10rem;
+  background: ${colors.red};
+  z-index: 0;
+  position: absolute;
+  right: 80px;
+  bottom: -30px;
+  @media (max-width: 900px) {
+    right: -20px;
+    bottom: -20px;
+    width: 60px;
+    height: 60px;
+  }
+  @media (max-width: 600px) {
+    right: -10px;
+    bottom: -10px;
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+const Circle = styled.div`
+  border: 1px solid ${colors.white};
+  border-radius: 50%;
+  background: transparent;
+  z-index: 0;
+  width: 10rem;
+  height: 10rem;
+  position: absolute;
+  left: 70px;
+  top: 180px;
+  @media (max-width: 900px) {
+    left: -40px;
+    top: 40px;
+    width: 90px;
+    height: 90px;
+  }
+  @media (max-width: 600px) {
+    left: -20px;
+    top: 20px;
+    width: 60px;
+    height: 60px;
+  }
 `;
--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,1 +1,151 @@
-export const RealEstateData = {};
+export const RealEstateData = {
+  logo: [
+    {
+      src: "",
+      alt: "",
+    },
+  ],
+  intro: "",
+  bannerImage: {
+    src: "",
+    alt: "",
+  },
+  brand: {
+    desc: "",
+    name: "",
+    location: "",
+  },
+  desc: ["", ""],
+  images: [{ src: "", alt: "" }],
+};
+
+export const FNBData = {
+  logo: [
+    {
+      src: "/assets/images/logo/f&b-logo.png",
+      alt: "F&B Logo",
+      isActive: true,
+    },
+  ],
+  intro:
+    "바쁜 일상 속에서 한신북어국은 최고의 북어국을 통해 든든하고 따뜻한 한끼를 제공합니다. 정성껏 우려낸 깊고 진한 국물, 신선한 재료, 균형 잡힌 영양으로 누구나 부담 없이 즐길 수 있는 건강한 한 그릇을 선보입니다.",
+  bannerImage: {
+    src: "/assets/images/projects/f&b-banner.jpg",
+    alt: "F&B Banner Image",
+  },
+  brand: {
+    desc: "한 그릇의 따뜻함, 정성을 담은",
+    name: "한신북어국",
+    location: "광명점",
+  },
+  isUl: true,
+  desc: [
+    "고품질 황태 사용 - 깊은 감칠맛과 담백한 맛의 조화, 믿을 수 있는 품질 관리",
+    "고단백·저지방 건강식 - 남녀노소 누구나 부담 없이 즐길 수 있는 균형 잡힌 영양식",
+    "최적의 온도와 시간으로 우려낸 국물 - 깊고 구수한 감칠맛 극대화",
+    "속을 편안하게 하는 깔끔한 맛 - 해장, 보양식, 부담 없는 식사로 제격",
+  ],
+  images: [
+    { src: "/assets/images/projects/f&b-1.jpg", alt: "F&B Image 1" },
+    { src: "/assets/images/projects/f&b-2.jpg", alt: "F&B Image 2" },
+    { src: "/assets/images/projects/f&b-3.jpg", alt: "F&B Image 3" },
+    { src: "/assets/images/projects/f&b-4.jpg", alt: "F&B Image 4" },
+  ],
+};
+
+export const EntertainmentData1 = {
+  logo: [
+    {
+      src: "/assets/images/logo/friends_screen-logo.jpeg",
+      alt: "Friends Screen Logo",
+      isActive: true,
+    },
+    {
+      src: "/assets/images/logo/cgv-logo.svg",
+      alt: "CGV Logo",
+      isActive: false,
+    },
+  ],
+  intro:
+    "친구, 가족, 동료와 함께 골프를 즐기며 친목을 다지고, 골프 실력을 향상시킬 수 있는 공간으로, 다양한 연령대와 수준의 고객에게 적합한 서비스를 제공합니다.",
+  bannerImage: {
+    src: "/assets/images/projects/enter1-banner.jpg",
+    alt: "Friends Screen Banner Image",
+  },
+  brand: {
+    desc: "실내에서 즐기는 리얼 골프",
+    name: "프렌즈스크린",
+    location: "GIDC광명역점",
+  },
+  isUl: false,
+  desc: [
+    "프렌즈스크린 GIDC광명역점은 최신 골프 시뮬레이터와 고해상도 스크린을 통해 실내에서도 실제골프와 같은 몰입감 있는 경험을 제공합니다.",
+    "GIDC광명역점은 광명역 인근에 위치해 있어 접근성이 뛰어나며, 최신 시설과 함께 편안한 분위기에서 골프의 매력을 만끽할 수 있는 공간입니다.",
+  ],
+  images: [
+    {
+      src: "/assets/images/projects/enter1-1.jpg",
+      alt: "Friends Screen Image 1",
+    },
+    {
+      src: "/assets/images/projects/enter1-2.jpg",
+      alt: "Friends Screen Image 2",
+    },
+    {
+      src: "/assets/images/projects/enter1-3.jpg",
+      alt: "Friends Screen Image 3",
+    },
+    {
+      src: "/assets/images/projects/enter1-4.jpg",
+      alt: "Friends Screen Image 4",
+    },
+  ],
+};
+
+export const EntertainmentData2 = {
+  logo: [
+    {
+      src: "/assets/images/logo/friends_screen-logo.jpeg",
+      alt: "Friends Screen Logo",
+      isActive: false,
+    },
+    {
+      src: "/assets/images/logo/cgv-logo.svg",
+      alt: "CGV Logo",
+      isActive: true,
+    },
+  ],
+  intro: "",
+  bannerImage: {
+    src: "/assets/images/projects/enter2-banner.jpg",
+    alt: "CGV Banner Image",
+  },
+  brand: {
+    desc: "첨단기술과 편안한 환경에서 최고의 경험을 제공하는",
+    name: "CGV",
+    location: "광명점",
+  },
+  isUl: false,
+  desc: [
+    "경기도 광명시 광명역에 위치한 최신 시설을 갖춘 멀티플렉스 영화관으로, 편리한 교통과 다양한 관람 경험을 제공합니다. 이 극장은 IMAX, 4DX, ScreenX등 CGV만의 첨단 상영관 시스템을 통해 관객들에게 몰입감 넘치는 영화 관람을 선사합니다.",
+    "영화와 함께 다양한 즐길 거리가 있는 CGV 광명역점은 지역 주민뿐만 아니라 광명역을 이용하는 많은 사람들에게 인기 있는 명소로 자리 잡고 있습니다.",
+  ],
+  images: [
+    {
+      src: "/assets/images/projects/enter2-1.jpg",
+      alt: "CGV Image 1",
+    },
+    {
+      src: "/assets/images/projects/enter2-2.jpg",
+      alt: "CGV Image 2",
+    },
+    {
+      src: "/assets/images/projects/enter2-3.jpg",
+      alt: "CGV Image 3",
+    },
+    {
+      src: "/assets/images/projects/enter2-4.jpg",
+      alt: "CGV Image 4",
+    },
+  ],
+};
